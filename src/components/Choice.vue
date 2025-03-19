e64ed4278ab8bc7201afb7ee44f6bf8a7bcdc0a5
msczk <maxime43200@gmail.com>
2025-03-19 18:23:14 +0100

    Gestion de la réponse correcte après séléction

--- src/components/Choice.vue
+++ src/components/Choice.vue
@@ -1,15 +1,34 @@
 <script setup>
-defineProps({
+import { computed } from 'vue'
+
+const props = defineProps({
   id: String,
-  value: String
+  value: String,
+  disabled: Boolean,
+  answer: String
 })
 
 const model = defineModel()
+
+const classes = computed(() => ({
+  right: props.disabled && props.value === props.answer,
+  wrong: props.disabled && props.value !== props.answer,
+}))
 </script>
 
 <template>
-  <label :for="id">
-    <input :id="id" name="answer" type="radio" :value="value" v-model="model">
+  <label :class="classes" :for="id">
+    <input  :disabled="disabled" :id="id" name="answer" type="radio" :value="value" v-model="model">
     {{value}}
   </label>
 </template>
+
+<style scoped>
+  .right{
+    color: green;
+  }
+
+  .wrong{
+    color: red;
+  }
+</style>
--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, ref, watch } from 'vue'
+import { computed, ref } from 'vue'
 import { shuffleArray } from '@/functions/array.js'
 import Choice from '@/components/Choice.vue'
 
@@ -10,6 +10,10 @@ const answer = ref(null)
 const emits = defineEmits(['answer'])
 
 const randomChoices = computed(() => shuffleArray(props.question.choices))
+
+const answered = computed(() => {
+  return answer.value !== null
+})
 </script>
 
 <template>
@@ -17,10 +21,10 @@ const randomChoices = computed(() => shuffleArray(props.question.choices))
     <h3>{{question.question}}</h3>
     <ul>
       <li v-for="(choice, index) in randomChoices" :key="choice">
-        <Choice :id="`answer-${index}`" :value="choice" v-model="answer" />
+        <Choice :disabled="answered" :answer="question.correct_answer" :id="`answer-${index}`" :value="choice" v-model="answer" />
       </li>
     </ul>
     <p v-if="answer">Votre réponse : {{answer}}</p>
-    <button :disabled="answer === null" @click="emits('answer', answer)">Suivant</button>
+    <button :disabled="!answered" @click="emits('answer', answer)">Suivant</button>
   </article>
 </template>
